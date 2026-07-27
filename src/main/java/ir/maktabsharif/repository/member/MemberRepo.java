520bd158d6102fa1414f156f567af0c58fe7898e
MhN_AR <mahan85a@gmail.com>
2026-07-27 16:22:17 +0330

    feat: extends MemberRepo from GenericRepo

---
+++ src/main/java/ir/maktabsharif/repository/member/MemberRepo.java
@@ -0,0 +1,7 @@
+package ir.maktabsharif.repository.member;
+
+import ir.maktabsharif.model.Member;
+import ir.maktabsharif.repository.GenericRepo;
+
+public interface MemberRepo extends GenericRepo<Member, Long> {
+}
