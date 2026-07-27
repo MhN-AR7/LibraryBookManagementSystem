890e820f36feb706d0b4c97b87f20041987f5bc8
MhN_AR <mahan85a@gmail.com>
2026-07-27 22:16:05 +0330

    feat: extends MemberService from GenericService

---
+++ src/main/java/ir/maktabsharif/service/member/MemberService.java
@@ -0,0 +1,7 @@
+package ir.maktabsharif.service.member;
+
+import ir.maktabsharif.model.Member;
+import ir.maktabsharif.service.GenericService;
+
+public interface MemberService extends GenericService<Member, Long> {
+}
