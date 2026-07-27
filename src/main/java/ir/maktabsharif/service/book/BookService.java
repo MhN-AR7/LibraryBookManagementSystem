a60316f9bd097876554daac22b324755a726093d
MhN_AR <mahan85a@gmail.com>
2026-07-27 22:15:21 +0330

    feat: extend BookService from GenericService

---
+++ src/main/java/ir/maktabsharif/service/book/BookService.java
@@ -0,0 +1,7 @@
+package ir.maktabsharif.service.book;
+
+import ir.maktabsharif.model.Book;
+import ir.maktabsharif.service.GenericService;
+
+public interface BookService extends GenericService<Book, Long> {
+}
