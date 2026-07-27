53b0df6c7a1743f8e5f451186e690fed6ae1ac3e
MhN_AR <mahan85a@gmail.com>
2026-07-27 16:21:41 +0330

    feat: extends BookRepo from GenericRepo

---
+++ src/main/java/ir/maktabsharif/repository/book/BookRepo.java
@@ -0,0 +1,7 @@
+package ir.maktabsharif.repository.book;
+
+import ir.maktabsharif.model.Book;
+import ir.maktabsharif.repository.GenericRepo;
+
+public interface BookRepo extends GenericRepo<Book, Long> {
+}
