bcb531c59cbcebcaea5f8a3479a2e0c774b1a750
MhN_AR <mahan85a@gmail.com>
2026-07-27 22:14:27 +0330

    feat: add Business Exceptions

---
+++ src/main/java/ir/maktabsharif/exception/InvalidDataException.java
@@ -0,0 +1,7 @@
+package ir.maktabsharif.exception;
+
+public class InvalidDataException extends BusinessException {
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
