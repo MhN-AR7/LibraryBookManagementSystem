2c73ced8ac6d1b4a76df97f85d1d6bd8c89958f1
MhN_AR <mahan85a@gmail.com>
2026-07-28 20:13:47 +0330

    fix: final edits

--- src/main/java/ir/maktabsharif/console/MainApp.java
+++ src/main/java/ir/maktabsharif/console/MainApp.java
@@ -14,6 +14,7 @@ import ir.maktabsharif.threads.Consumer;
 import ir.maktabsharif.threads.Producer;
 import ir.maktabsharif.threads.ThreadPool;
 import ir.maktabsharif.threads.Warehouse;
+import ir.maktabsharif.util.JpaConfig;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -309,6 +310,7 @@ public class MainApp {
                 case 0:
                     System.out.println("Exiting Program...");
                     input.close();
+                    JpaConfig.closeEMF();
                     return;
                 default:
                     System.out.println("Invalid Choice!\nTry Again.");
