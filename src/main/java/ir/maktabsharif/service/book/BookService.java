7a15238e41b2dbadb25e3fee5ab82d2b6676dbab
MhN_AR <mahan85a@gmail.com>
2026-07-28 16:16:12 +0330

    feat: add borrow method

--- src/main/java/ir/maktabsharif/service/book/BookService.java
+++ src/main/java/ir/maktabsharif/service/book/BookService.java
@@ -1,7 +1,9 @@
 package ir.maktabsharif.service.book;
 
+import ir.maktabsharif.exception.BookNotFoundException;
 import ir.maktabsharif.model.Book;
 import ir.maktabsharif.service.GenericService;
 
 public interface BookService extends GenericService<Book, Long> {
+    void borrow(Long id) throws BookNotFoundException;
 }
